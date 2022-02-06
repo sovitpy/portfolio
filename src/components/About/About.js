701aa91e765af0b4e4f3d5b0df204291954e652c
sovitpy <sovitlekhak@gmail.com>
2022-02-06 17:48:16 +0545

    Added pic

--- src/components/About/About.js
+++ src/components/About/About.js
@@ -2,22 +2,30 @@ import GitHubIcon from '@material-ui/icons/GitHub'
 import LinkedInIcon from '@material-ui/icons/LinkedIn'
 import { about } from '../../portfolio'
 import './About.css'
+import sovit from './sovit.jpg'
 
 const About = () => {
   const { name, role, description, resume, social } = about
 
   return (
     <div className='about center'>
+      <div className='about__bio'>
+      <div className='image-container'>
+      <img src={sovit} alt="me" className='about__image' />
+      </div>
+      <div className='about__right'>
       {name && (
         <h1>
           Hi, I am <span className='about__name'>{name}.</span>
         </h1>
       )}
-
+      
       {role && <h2 className='about__role'>A {role}.</h2>}
       <p className='about__desc'>{description && description}</p>
+      </div>
+      </div>
 
-      <div className='about__contact center'>
+      <div className='about__contact minus-margin center'>
         {resume && (
           <a href={resume}>
             <span type='button' className='btn btn--outline'>
