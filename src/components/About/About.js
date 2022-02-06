991b4be1784d002ba203daec11c556ba4c257d39
sovitpy <sovitlekhak@gmail.com>
2022-02-07 04:17:52 +0545

    Added Projects

--- src/components/About/About.js
+++ src/components/About/About.js
@@ -2,7 +2,7 @@ import GitHubIcon from '@material-ui/icons/GitHub'
 import LinkedInIcon from '@material-ui/icons/LinkedIn'
 import { about } from '../../portfolio'
 import './About.css'
-import sovit from './sovit.jpg'
+import sovit from './sovit.png'
 
 const About = () => {
   const { name, role, description, resume, social } = about
